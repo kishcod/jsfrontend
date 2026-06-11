6da2546766afec5103a0e447ed19ff5997fadd13
html <147909829+kishcod@users.noreply.github.com>
2026-06-11 20:19:41 +0300

    Create Users.jsx

---
+++ src/pages/Users.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "../components/Sidebar";
+
+export default function Users() {
+  return (
+    <div className="layout">
+
+      <Sidebar />
+
+      <div className="content">
+        <h2>Users</h2>
+      </div>
+
+    </div>
+  );
+}
