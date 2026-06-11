d7e055bd0403d96c04e825ebd6262c1a1fabe929
html <147909829+kishcod@users.noreply.github.com>
2026-06-11 20:21:55 +0300

    Create Vouchers.jsx

---
+++ src/pages/Vouchers.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "../components/Sidebar";
+
+export default function Vouchers() {
+  return (
+    <div className="layout">
+
+      <Sidebar />
+
+      <div className="content">
+        <h2>Voucher Generator</h2>
+      </div>
+
+    </div>
+  );
+}
