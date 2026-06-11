082d1048ac33cf2db964807271cd03cb769ac007
html <147909829+kishcod@users.noreply.github.com>
2026-06-11 20:22:28 +0300

    Create Settings.jsx

---
+++ src/pages/Settings.jsx
@@ -0,0 +1,15 @@
+import Sidebar from "../components/Sidebar";
+
+export default function Settings() {
+  return (
+    <div className="layout">
+
+      <Sidebar />
+
+      <div className="content">
+        <h2>Settings</h2>
+      </div>
+
+    </div>
+  );
+}
