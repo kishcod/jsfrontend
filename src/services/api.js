052b8928229e120ad306e267cd6902a400ba2b54
html <147909829+kishcod@users.noreply.github.com>
2026-06-11 20:02:13 +0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,19 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+const API = axios.create({
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://josmawifi.onrender.com"
 });
 
-export default api;
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export default API;
